41ff3653f65eba46ba2cbc4914d3335085d714b9
Jose Pablo Garbanzo <jpgarbanzo@gmail.com>
2025-05-12 18:00:21 -0600

    Fixing example form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-intro';
+
+  person = {
+    name: '',
+    age: null
+  };
+
+  onButtonClick() {
+    console.log(`Person Info: Name - ${this.person.name}, Age - ${this.person.age}`);
+  }
 }
