5b35b207e4f68eabc09f18ed0118340c1a26701a
Jose Pablo Garbanzo <jpgarbanzo@gmail.com>
2025-05-14 20:27:21 -0600

    Adding forms input to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ExampleComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppComponent,
+    ExampleComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
