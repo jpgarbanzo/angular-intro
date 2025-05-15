292434cef1dd1a05562fd73bcdc4243b8f9a6698
Jose Pablo Garbanzo <jpgarbanzo@gmail.com>
2025-05-14 21:28:39 -0600

    Adding example page with form validations

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-example',
   templateUrl: './example.component.html',
-  styleUrls: ['./example.component.css']
+  imports: [ReactiveFormsModule],
 })
-export class ExampleComponent {}
+export class ExampleComponent {
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^[^@\\s]+@[^@\\s]+\\.com$'),
+    ]),
+    identification: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+  });
+
+  onSubmit() {
+    console.log(this.profileForm.value);
+  }
+}
